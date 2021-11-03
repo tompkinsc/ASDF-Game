423f382a613957990ac1bda9f208ef3cd3bcc972
tompkinsc <ctompki@iu.edu>
2021-11-03 17:44:39 -0400

    Update 1
    Added player animations, added monster, added monster animations and movescript

---
+++ Assets/MonsterMovement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonsterMovement : MonoBehaviour
+{
+    private Rigidbody2D rb;
+
+    public float moveSpeed;
+
+    private Vector3 moveDir = new Vector3(0, 1).normalized;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = moveDir * moveSpeed * Time.deltaTime;
+    }
+
+    private void StopMoving()
+    {
+        rb.velocity = Vector3.zero;
+    }
+}
